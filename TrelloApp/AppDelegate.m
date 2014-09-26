2f2542bcc8b126a71c13c27eeac5e53ab63755fb
Ryan Lee <hi@iamryanlee.com>
2014-09-26 17:32:20 +0800

    Added minimum window width and height.

--- TrelloApp/AppDelegate.m
+++ TrelloApp/AppDelegate.m
@@ -16,15 +16,18 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    NSRect frame=[NSScreen mainScreen].frame ;
-    [self.window setFrame:frame display:YES animate:YES];
+    //set content to webview
+    [self.window setContentView:self.webView];
+    [self.window setTitle:@"TrelloApp"];
+    
+    //go to trello.com
     NSURL *url = [NSURL URLWithString:@"https://trello.com"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     [[[self webView] mainFrame] loadRequest:urlRequest];
+    
+    //fullscreen mode support
     [self.window setCollectionBehavior:
      NSWindowCollectionBehaviorFullScreenPrimary];
-    [self.window setContentView:self.webView];
-    [self.window setTitle:@"TrelloApp"];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
